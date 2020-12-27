8a7674d39844de3e3aa74de0ed42f8f5bf57525e
Mario Cerven <mario.cerven@panaxeo.com>
2020-12-27 17:52:43 +0100

    Moved Layout component to Layouts folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import Layout from '../components/Layout';
+import Layout from '../layouts';
 
 export default function MyApp({ Component, pageProps }) {
   return (
