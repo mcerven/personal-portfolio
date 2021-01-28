9b0c439258d873e3ff81cc4753b64de6ede3f20b
Mario Cerven <mario.cerven@panaxeo.com>
2021-01-28 17:27:29 +0100

    Add scroll margin

--- components/SectionWrapper/index.js
+++ components/SectionWrapper/index.js
@@ -4,13 +4,10 @@ import styles from './index.module.css';
 
 export default function SectionWrapper({ id, title, fullWidth, children }) {
   return (
-    <div className={`mt-20 w-full ${!fullWidth && 'lg:pr-10 lg:w-1/2'}`}>
-      <div id={id} className={`${styles.sectionAnchorOffset} absolute w-0 h-0`}></div>
-      <section>
-        <h2 className="text-5xl font-light tracking-wide text-accent pb-5">{title}</h2>
-        {children}
-      </section>
-    </div>
+    <section id={id} className={`${styles.SectionWrapper} w-full ${!fullWidth && 'lg:pr-10 lg:w-1/2'}`}>
+      <h2 className="text-5xl font-light tracking-wide text-accent pb-5">{title}</h2>
+      {children}
+    </section>
   );
 }
 
