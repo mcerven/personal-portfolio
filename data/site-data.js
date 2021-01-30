622e085742999be6da37e7db3669bd595438b50c
Mario Cerven <mario.cerven@panaxeo.com>
2021-01-30 15:30:15 +0100

    Updated text

--- data/site-data.js
+++ data/site-data.js
@@ -1,6 +1,6 @@
 export const authorName = "Mario Cerven";
 export const githubName = "mcerven";
-export const profileText = `I'm ${authorName} and I am a web developer. I work remotely from Slovakia.`;
+export const profileText = `I'm ${authorName} and I am a fullstack web developer. In my free time I enjoy learning new technologies.`;
 export const footerText = `© ${new Date().getFullYear()} - ${authorName}`;
 
 export const experienceItems = [
