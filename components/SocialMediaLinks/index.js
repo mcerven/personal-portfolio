0b09ec8124d942eadc42c8535bdbd9f4458f2c69
Mario Cerven <mario.cerven@panaxeo.com>
2021-03-24 00:22:59 +0100

    Added CSS Battle link

--- components/SocialMediaLinks/index.js
+++ components/SocialMediaLinks/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import SocialMediaLink from './SocialMediaLink';
 import LinkedinIcon from './LinkedinIcon';
 import GithubIcon from './GithubIcon';
+import CssBattleIcon from './CssBattleIcon';
 
 export default function SocialMediaLinks() {
   return (
     <div className="flex flex-wrap space-x-2.5">
       <SocialMediaLink title="LinkedIn" href="https://linkedin.com/in/mariocerven">
-        <LinkedinIcon />
+        <LinkedinIcon style={{ width: '1em' }} />
       </SocialMediaLink>
       <SocialMediaLink title="GitHub" href="https://github.com/mcerven">
-        <GithubIcon />
+        <GithubIcon style={{ width: '1em' }} />
+      </SocialMediaLink>
+      <SocialMediaLink title="CSS Battle" href="https://cssbattle.dev/player/mcerven">
+        <CssBattleIcon style={{ width: '1em' }} />
       </SocialMediaLink>
     </div>
   );
